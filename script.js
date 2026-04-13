25403a5a17af8770fef5c3d823b0a11c5e76010e
weghnersoares-del <weghner.soares@escola.pr.gov.br>
2026-04-13 07:44:27 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,11 @@
+//pegar os elementos do HTML
+const form = document.getElementById("loginform");
+const menssege = document.getElementById("menssege");
+
+//evendo de envio do formulario
+
+form.addEventListener("submit", function(event)) {
+    Event.preventdefault();//impede o carregamento da pagina
+
+
+}
